4593c6caa6ed1842ad093d3c42079f234eaedef6
ElielNascimento <nascimentob.eliel@gmail.com>
2022-01-20 06:54:56 -0300

    Save score

--- backend/dsmovie/src/main/java/com/devsuperior/dsmovie/controllers/MovieController.java
+++ backend/dsmovie/src/main/java/com/devsuperior/dsmovie/controllers/MovieController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsmovie.dto.MovieDTO;
-import com.devsuperior.dsmovie.service.MovieService;
+import com.devsuperior.dsmovie.services.MovieService;
 
 @RestController
 @RequestMapping("/movies")
